785e9feb9e5cc2d3eea6b2b2a4fa3a6bad100327
Klaudiusz  Wiktor <klaudiusz.wiktor1@gmail.com>
2023-01-27 23:01:17 +0100

    FirsCommit

--- src/Components/creditCalc/CreditCalc.jsx
+++ src/Components/creditCalc/CreditCalc.jsx
@@ -1,5 +1,35 @@
 const CreditCalc = () => {
-  return <div>Kalkulator kredytu</div>;
+  return (
+    <div>
+      <form>
+        <label htmlFor="credit">
+          Ile chciałbyś otrzymać od banku?
+          <input type="number" id="credit" /> zł
+        </label>
+
+        <label htmlFor="date">
+          Do kiedy chcesz spłacać kredyt?
+          <input type="date" id="date" />
+        </label>
+
+        <label htmlFor="rrso">
+          Podaj RRSO banku
+          <input type="text" id="rrso" /> %
+        </label>
+
+        <label htmlFor="commission">
+          Czy bank pobiera prowizje?
+          <select id="commission">
+            <option value="yes-top">Tak i jest płatna z góry</option>
+            <option value="yes-bottom">Tak i jest płatna z dołu</option>
+            <option value="no">Nie</option>
+          </select>
+          ile wynosi?
+          <input type="number" />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default CreditCalc;
