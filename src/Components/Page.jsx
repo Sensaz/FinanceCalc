77c94c51ccb295145a02368053f500ae994151f7
Klaudiusz  Wiktor <klaudiusz.wiktor1@gmail.com>
2023-02-03 23:51:50 +0100

    fix react router swap link

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -1,4 +1,6 @@
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+
 import CompanyValutionCalc from "./companyValutionCalc/CompanyValutionCalc";
 import CompoundInterestCalc from "./compoundInterestCalc/CompoundInterestCalc";
 import CreditCalc from "./creditCalc/CreditCalc";
@@ -6,13 +8,31 @@ import HomePage from "./HomePage";
 import ErrorPage from "./ErrorPage";
 
 const Page = () => {
+  const location = useLocation();
+  const negative = useNavigate();
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        return negative("/");
+      case "/companyValutionCalc":
+        return negative("/companyValutionCalc");
+      case "/compoundInterestCalc":
+        return negative("/compoundInterestCalc");
+      case "/creditCalc":
+        return negative("/creditCalc");
+      default:
+        negative("/error");
+    }
+  }, [negative]);
+
   return (
     <Routes>
       <Route path="/" exact element={<HomePage />} />
       <Route path="/companyValutionCalc" element={<CompanyValutionCalc />} />
       <Route path="/compoundInterestCalc" element={<CompoundInterestCalc />} />
       <Route path="/creditCalc" element={<CreditCalc />} />
-      <Route path="*" element={<ErrorPage />} />
+      <Route path="/error" element={<ErrorPage />} />
     </Routes>
   );
 };
